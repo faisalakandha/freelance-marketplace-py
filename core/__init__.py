2f9d2e925cbaf164964e47ddba2540cb6d778624
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2023-02-28 08:11:15 +0600

    Authentication Not completed

--- core/__init__.py
+++ core/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from core import views, models
